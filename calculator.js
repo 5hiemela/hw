d8fe8174a07ce829cb852a846cbaac9b0916931a
Chiemela <onyensochiemela24@gmail.com>
2026-09-15 13:33:49 -0400

    Add comments

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
 let continueLoop = true;
 
+// Create a table to display the results
 document.write("<table>");
 document.write("<tr><th>x</th><th>op</th><th>y</th><th>result</th></tr>");
 
+// Loop until the user clicks "Cancel"
 while (continueLoop) {
     let x = prompt("Enter the first number:");
     let y = prompt("Enter the second number:");
@@ -11,6 +13,7 @@ while (continueLoop) {
 
     let result;
 
+    // Checks if inputs are valid numbers & calculates the result based on operator
     if (isNaN(x) || isNaN(y)) {
         result = "Invalid input number";
     } else if (operator === "+") {
@@ -27,9 +30,11 @@ while (continueLoop) {
         result = "Invalid operator"
     }
 
+    // Display the result in the table
     document.write("<tr><td>" + x + "</td><td>" + operator + "</td><td>" + y + "</td><td>" + result + "</td></tr>");
 
     continueLoop = confirm("Click OK to continue or Cancel to exit.");
 }
 
+// Close the table
 document.write("</table>");
