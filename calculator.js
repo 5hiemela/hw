287ab438b77f02b0d738f9aa121355f68fdcdb24
Chiemela <onyensochiemela24@gmail.com>
2026-09-15 13:00:15 -0400

    Add while loop to calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,10 @@
-let x = prompt("Enter the first number:");
-let y = prompt("Enter the second number:");
+let continueLoop = true;
 
-let operator = prompt("Enter an operator (+, -, *, /, %):");
+while (continueLoop) {
+    let x = prompt("Enter the first number:");
+    let y = prompt("Enter the second number:");
+
+    let operator = prompt("Enter an operator (+, -, *, /, %):");
+
+    continueLoop = confirm("Click OK to continue or Cancel to exit.");
+}
